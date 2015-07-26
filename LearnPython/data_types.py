be53ac7293ff90a9f89e541b735608c8a88d7572
lagrassa <alex.lagrassa@gmail.com>
2015-07-25 22:06:38 -0400

    functioning game with one bubble

--- LearnPython/data_types.py
+++ LearnPython/data_types.py
@@ -1,26 +1,72 @@
 import pygame
 pygame.init()
-screen = pygame.display.set_mode((600, 500), 0, 32)
+screen_height = 700
+screen_width = 500
+
+font = pygame.font.SysFont('Arial', 25)
+small_font = pygame.font.SysFont('Arial',18)
+screen = pygame.display.set_mode((screen_height, screen_width), 0, 32)
+screen.fill((255,255,255))
 mainClock = pygame.time.Clock()
 class Bubble():
+    def __init__(self):
+        self.examples = ["{'a':5, 'b':6}","[Jess, Alex]","3.0/2","3/2","'Hello'", "(1,5)"]
+        self.example_data_types = ["dictionary","list","float","int","string","tuple"]
+        self.current_example_index = 0
+        self.current_example = self.examples[0]
+        self.radius = 60
     def draw_bubble(self):
-        circleRect = pygame.draw.circle(screen, (255, 255, 255), (100, 200), 40)
-
+        circleRect = pygame.draw.circle(screen, (10, 60, 30), (100, 200), 60)
+    def change_displayed_data(self):
+        self.current_example_index +=1
+        self.current_example = self.examples[self.current_example_index]
+    def addText(self):
+        screen.blit(small_font.render(self.current_example, True, (255,0,0)), (50,190))
+    def is_in_range(self,(x,y)):
+        print "x", x
+        x_circle= 100
+        y_circle= 200
+        if ((x-x_circle)**2+(y-y_circle)**2)**0.5 < 60:
+            return True
+        return False
+    def is_correct(self, data_displayer):
+        bubble_type = self.example_data_types[self.current_example_index]
+        correct_type = data_displayer.current_data_type
+        if bubble_type == correct_type:
+            return True
+        return False
 class Data_Displayer():
     def __init__(self):
-        self.font = pygame.font.SysFont('Arial', 25)
-        pygame.display.set_caption('Box Test')
+        pygame.display.set_caption('Data Types')
         pygame.display.update()
+        self.data_types= ["dictionary", "list", "float", "int", "string", "tuple"]
+        self.current_data_type_index = 0
+        self.current_data_type = "dictionary"
     def draw_self(self):
-        self.rect = pygame.draw.rect(screen, (50,50,50), (175, 75, 200, 100), 2)
+        self.rect = pygame.draw.rect(screen, (25,25,25), (200, 350, screen_width/2.0, screen_height/5.0), 2)
     def addText(self):
-        screen.blit(self.font.render('Hello!', True, (255,0,0)), (200, 100))
+        screen.blit(font.render(self.current_data_type, True, (255,0,0)), (250, 375))
+
         pygame.display.update()
+    def change(self):
+        self.current_data_type_index+=1
+        self.current_data_type = self.data_types[self.current_data_type_index]
+        
 b = Bubble()
 d= Data_Displayer()
 while True:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            position = pygame.mouse.get_pos()
+            if b.is_in_range(position):
+                if b.is_correct(d):
+                    d.change()
+                    b.change_displayed_data()
+    
+    screen.fill((255,255,255))
     b.draw_bubble()
     d.draw_self()
     d.addText()
+    b.addText()
     pygame.display.update()
     mainClock.tick(40)
